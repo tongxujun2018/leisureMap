74ddb3ca1bd8f9a7501ece5e5311e2957c083dd9
stu1 <35651865@qq.com>
2018-07-28 17:52:24 +0800

    0728-03

--- leisureMap/MapFlow/mapViewController.swift
+++ leisureMap/MapFlow/mapViewController.swift
@@ -7,15 +7,113 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class mapViewController: UIViewController {
 
+
+
+
+
+class mapViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    let regionRadius: CLLocationDistance =  1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            
+            locationManager.delegate = self
+            
+            locationManager.desiredAccuracy =
+kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+            
+            
+            }
+        
+        
+        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+   
+        //centerMapOnLocation(loction.coordinate,)
+        
+        
+//        let flag = MapFlag(title:"ios app by swift",locationName:"abc",discipline:"apple room",coordinate:CLLocationCoordinate2D(latitude:31.29065118 , longitude: 118.368044))
+        
+        let flag = MapFlag(title: "ios app by swift", locationName: "abc", discipline: "apple room", coordinate: CLLocationCoordinate2D(latitude:31.29075118 , longitude: 118.3625587), url: "https://apple.com")
+        
+        mapView.addAnnotation(flag)
+        
         // Do any additional setup after loading the view.
     }
-
+  
+    func centerMapOnLocation(loction: CLLocation){
+        
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(loction.coordinate, regionRadius,regionRadius)
+        
+        mapView.setRegion(coordinateRegion,animated:true)
+        
+        
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+       
+        guard let locValue :  CLLocationCoordinate2D = manager.location?.coordinate  else{
+           
+            return
+            
+        }
+        
+        print("latitude:\(locValue.latitude):longitude:\(locValue.longitude)")
+        centerMapOnLocation(loction: manager.location!)
+        
+    }
+    
+    @objc func moveToWebView(sender : UIButton ){
+        
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard let annotation = annotation as? MapFlag else{
+            return nil
+        }
+        
+        
+        let identifier = "marker"
+        let annotationView : MKAnnotationView
+        
+        if let dequeueView =  mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKMarkerAnnotationView{
+            
+            dequeueView.annotation = annotation
+            annotationView = dequeueView
+            
+        }else{
+            
+            annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView.canShowCallout=true
+            annotationView.calloutOffset = CGPoint(x: -5, y: 5)
+            
+            let button = UIButton(type: .detailDisclosure)
+            annotationView.rightCalloutAccessoryView = button
+            
+            button.addTarget(self, action: #selector(self.moveToWebView(sender:)), for: .touchUpInside)
+        }
+        return annotationView
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- leisureMap/Master-detail/detailViewController.swift
+++ leisureMap/Master-detail/detailViewController.swift
@@ -25,20 +25,39 @@ class detailViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func btnmapclicked(_ sender: Any) {
+        DispatchQueue.main.async {
+            self.performSegue(withIdentifier: "movetomap", sender: self)
+        }
+    }
+    @IBAction func btnwebclicked(_ sender: Any) {
+        DispatchQueue.main.async {
+            self.performSegue(withIdentifier: "movetonote", sender: self)
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
 
-    /*
+ 
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        switch segue.identifier {
+        case "movetomap":
+            
+            break
+            
+        case "movetonote":
+            
+            break
+        default:
+            break
+        }
     }
-    */
+   
 
 }
