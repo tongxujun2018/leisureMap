ac4fd2e75b518bf6451d94f77e3ee1e8308087a2
stu1 <35651865@qq.com>
2018-07-29 11:36:06 +0800

    20180729-2

--- leisureMap/NoteFlow/noteViewController.swift
+++ leisureMap/NoteFlow/noteViewController.swift
@@ -24,27 +24,27 @@ class noteViewController: UIViewController {
     
     @IBOutlet weak var btnPhoto: UIButton!
     
-    func camera()
-    {
-        if UIImagePickerController.isSourceTypeAvailable(.camera){
-            let myPickerController = UIImagePickerController()
-            myPickerController.delegate = self ;
-            myPickerController.sourceType = .camera
-            self.present(myPickerController, animated: true, completion: nil)
-        }
-        
-    }
-    
-    func photoLibrary()
-    {
-        
-        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
-            let myPickerController = UIImagePickerController()
-            myPickerController.delegate = self ;
-            myPickerController.sourceType = .photoLibrary
-            self.present(myPickerController, animated: true, completion: nil)
-        }
-    }
+//    func camera()
+//    {
+//        if UIImagePickerController.isSourceTypeAvailable(.camera){
+//            let myPickerController = UIImagePickerController()
+//            myPickerController.delegate = self ;
+//            myPickerController.sourceType = .camera
+//            self.present(myPickerController, animated: true, completion: nil)
+//        }
+//        
+//    }
+//    
+//    func photoLibrary()
+//    {
+//        
+//        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
+//            let myPickerController = UIImagePickerController()
+//            myPickerController.delegate = self ;
+//            myPickerController.sourceType = .photoLibrary
+//            self.present(myPickerController, animated: true, completion: nil)
+//        }
+//    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
